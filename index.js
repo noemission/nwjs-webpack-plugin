87de711636ded1d2470fbc13c19810eb918a2442
alex-rokabilis <alekos_l2@hotmail.com>
2019-01-19 19:25:48 +0200

    initial

---
+++ index.js
@@ -0,0 +1,80 @@
+const net = require('net')
+const getPort = require('get-port');
+const path = require('path')
+const SingleEntryPlugin = require('webpack/lib/SingleEntryPlugin')
+const DefinePlugin = require('webpack/lib/DefinePlugin')
+
+module.exports = class NwJSPlugin {
+    constructor() {
+        this.portPromise = getPort();
+        this.sockets = [];
+
+        this.portPromise.then((port) => {
+            net.createServer((socket) => {
+                this.sockets.push(socket);
+                if (this.lastErrorMessage) {
+                    socket.write(JSON.stringify(this.lastErrorMessage))
+                }
+            })
+                .listen(port)
+                .on('listening', () => console.log('listening on ' + port))
+        })
+
+
+    }
+    runNW() {
+        if (this.nw_instance && this.nw_instance.pid) return;
+        this.nw_instance = require('child_process').spawn('nw', [process.cwd()], {
+            stdio: "inherit"
+        });
+    }
+    write(message) {
+        this.sockets.forEach(socket => socket.write(JSON.stringify(message)))
+        if (message.status === 'ERROR') {
+            this.lastErrorMessage = message;
+        } else {
+            this.lastErrorMessage = null;
+        }
+    }
+    apply(compiler) {
+
+        const definePort = (port) => {
+            new DefinePlugin({ 'THE_PORT': port }).apply(compiler);
+            return true;
+        }
+
+        compiler.hooks.watchRun.tapPromise('NwJSPlugin', c => this.portPromise.then(definePort));
+        compiler.hooks.run.tapPromise('NwJSPlugin', c => this.portPromise.then(definePort));
+
+
+        compiler.hooks.entryOption.tap('NwJSPlugin', () => {
+
+            new SingleEntryPlugin(__dirname, path.resolve(__dirname, 'reloader.js'), 'reloader').apply(compiler)
+        })
+
+        compiler.hooks.watchRun.tap('NwJSPlugin', () => {
+            this.write({
+                status: "WATCH_RUN"
+            })
+        });
+
+        compiler.hooks.done.tap('NwJSPlugin', (stats) => {
+            this.runNW();
+            if (stats.compilation.errors.length) {
+                this.write({
+                    status: "ERROR",
+                    errors: stats.compilation.errors.map(err => ({
+                        file: err.module.id,
+                        location: err.loc,
+                        toEval: err.module._source._value,
+                    }))
+                })
+            } else {
+                this.write({
+                    status: "DONE",
+                })
+            }
+        });
+
+    }
+}
---
+++ reloader.js
@@ -0,0 +1,66 @@
+const net = require('net');
+
+const randomNumber = Math.random() * 10e9 | 0;
+const randomID = `webpack-error-message-${randomNumber}`
+const client = net.connect({ port: THE_PORT }, () => console.log('Live reload connected!'))
+
+const css = `
+#${randomID}{
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    color: rgb(222, 85, 85);
+    margin: 0px;
+    font-size: 1.3em;
+}
+`
+const style = document.createElement('style');
+style.type = 'text/css';
+style.appendChild(document.createTextNode(css))
+document.head.appendChild(style)
+
+
+client.on('data', (data) => {
+    data = JSON.parse(data.toString())
+    console.log("Server:", data);
+    if (data.status === 'DONE') {
+        location.reload();
+    }
+    if (data.status === 'ERROR') {
+        data.errors.forEach(err => {
+            try {
+                eval(err.toEval)
+            } catch (error) {
+                let el = document.getElementById(randomID);
+                const header = [
+                    `ERROR in ${err.file} ${err.location.line}:${err.location.column}`,
+                    ...error.message.split('\n').slice(0, 2)
+                ]
+                const body = error.message.split('\n').slice(2)
+
+                el && el.remove();
+                el = document.createElement('div')
+                el.id = randomID
+                
+                const headerEl = document.createElement('pre')
+                headerEl.innerText = header.join('\n');
+                headerEl.style.fontWeight = 'bold'
+                headerEl.style.marginLeft = '0.5em'
+
+                const bodyEl = document.createElement('pre')
+                bodyEl.innerText = body.join('\n');
+                bodyEl.style.marginLeft = '1em'
+
+                el.append(headerEl)
+                el.append(bodyEl)
+
+                document.body.append(el);
+                console.error(error.message)
+            }
+        });
+    }
+    nw.Window.get().focus();
+});
